771dd5285bf454415eede64715b979c414849e5e
Zelim <anzorov.zelim@gmail.com>
2022-05-18 16:26:11 +0300

    object soldier created, added three empty methods

---
+++ soldier.js
@@ -0,0 +1,12 @@
+const soldier = {
+  name: "John",
+  health: 10,
+  weapon: {
+    name: "AK47",
+    rounds: 30,
+  },
+  ammo: 3,
+  shoot: function () {},
+  reload: function () {},
+  wound: function () {},
+};
