facbe8ddc54c50460dc26463d776c054a3bcca71
Zelim <anzorov.zelim@gmail.com>
2022-05-18 17:38:04 +0300

    modified three methods

--- soldier.js
+++ soldier.js
@@ -6,7 +6,22 @@ const soldier = {
     rounds: 30,
   },
   ammo: 3,
-  shoot: function () {},
-  reload: function () {},
-  wound: function () {},
+  shoot: function () {
+    --soldier.weapon.rounds;
+    console.log("бах-бах");
+  },
+  reload: function () {
+    soldier.weapon.rounds = 30;
+    --soldier.ammo;
+    console.log("перезарядка...");
+    if (soldier.ammo == 0) {
+      console.log("не осталось припасов");
+    }
+  },
+  wound: function () {
+    --soldier.health;
+    if (soldier.health == 0) {
+      console.log("Ты проиграл! " + "Здоровье: " + soldier.health);
+    }
+  },
 };
